cfda413766d664e74038275ac9a1a7c7faeb0fd5
beeplin <beeplin@gmail.com>
2025-11-27 23:07:03 +0800

    aa

--- keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
+++ keyboards/keychron/k9_pro/ansi/rgb/keymaps/via/keymap.c
@@ -9,24 +9,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LGUI,  KC_LALT,                LT(2, KC_SPC),                                KC_RALT,  KC_RGUI,  KC_RCTL,      LT(4, KC_APP)
     ),
     [1] = LAYOUT_61_ansi(
-        KC_F11,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_BRID,  KC_BRIU,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPRV,  KC_MNXT,  KC_MPLY,
-        KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_K,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_SCRL,  KC_PAUS,
-        KC_LCTL,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_PSCR,
+        KC_F11,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_BRID,  KC_BRIU,  KC_UNDS,  KC_LPRN,  KC_LCBR,  KC_RCBR,  KC_RPRN,  KC_BSPC,
+        KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_K,     KC_L,     KC_U,     KC_Y,     KC_SCLN,  KC_LBRC,  KC_RBRC,  KC_BSLS,
+ LCTL_T(KC_CAPS), KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,            KC_ENT,
         _______,  KC_X,     KC_C,     KC_D,     KC_Z,     KC_V,     KC_J,     KC_H,     KC_COMM,  KC_DOT,   KC_SLSH,                      _______,
-  LT(3, KC_CAPS), _______,  _______,                      _______,                                _______,  _______,  _______,            _______
+  LT(3, KC_LCTL), _______,  _______,                      _______,                                _______,  _______,  _______,            _______
     ),
     [2] = LAYOUT_61_ansi(
-        KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_INS,   KC_APP,   KC_BTN1,  KC_BTN3,  KC_BTN2,  KC_BTN4,  KC_BTN5,  KC_WH_U,
-        KC_SPC,   KC_ESC,   KC_BSPC,  KC_UP,    KC_DEL,   KC_PGUP,  KC_HOME,  KC_GRV,   KC_MINS,  KC_EQL,   KC_BSLS,  KC_RBRC,  KC_WH_L,  KC_WH_R,
-        _______,  KC_ENT,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,   KC_GT,    KC_LCBR,  KC_LPRN,  KC_UNDS,  KC_PIPE,            KC_WH_D,
+        KC_F12,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_PSCR,  KC_SCRL,  KC_PAUS,  KC_WH_L,  KC_WH_R,  KC_VOLD,  KC_VOLU,  KC_MUTE,
+        KC_LALT,  KC_ESC,   KC_BSPC,  KC_UP,    KC_DEL,   KC_PGUP,  KC_HOME,  KC_INS,   KC_LPRN,  KC_LCBR,  CW_TOGG,  KC_MPRV,  KC_MNXT,  KC_MPLY,
+        _______,  KC_ENT,   KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_PGDN,  KC_END,   KC_GT,    KC_MINS,  KC_EQL,   KC_BSLS,  KC_GRV,             KC_SPC,
         _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,                         _______,
         _______,  _______,  _______,                      _______,                                _______,  _______,  _______,            KC_SLEP
     ),
     [3] = LAYOUT_61_ansi(
-        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_APP,
-        KC_INS,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SCRL,  KC_MPRV,  KC_BTN4,  KC_WH_U,  KC_BTN5,  KC_VOLU,  KC_BRIU,  KC_SCRL,  KC_PAUS,
-        _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_PSCR,  KC_MNXT,  KC_WH_L,  KC_WH_D,  KC_WH_R,  KC_VOLD,  KC_BRID,            KC_PSCR,
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_PAUS,  KC_MPLY,  KC_BTN1,  KC_BTN3,  KC_BTN2,  KC_MUTE,                      _______,
+        KC_GRV,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_PSCR,
+        KC_APP,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_SCRL,  KC_MPRV,  KC_BTN4,  KC_WH_U,  KC_BTN5,  KC_VOLU,  KC_BRIU,  KC_SCRL,  KC_PAUS,
+        _______,  KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_PAUS,  KC_MNXT,  KC_WH_L,  KC_WH_D,  KC_WH_R,  KC_VOLD,  KC_BRID,            KC_APP,
+        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_PSCR,  KC_MPLY,  KC_BTN1,  KC_BTN3,  KC_BTN2,  KC_MUTE,                      _______,
         _______,  _______,  _______,                      CW_TOGG,                                _______,  _______,  _______,            _______
     ),
     [4] = LAYOUT_61_ansi(
